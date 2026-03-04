5ce6e998ea8f30a91914a58fc3e29bc5d85676bb
rajeswari-29 <rajeswari.bsc29@gmail.com>
2026-03-04 10:09:20 +0530

    Install React Router

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,10 @@
-import { Routes, Route } from 'react-router-dom'
-import Navbar from './components/Navbar'
-import Home from './pages/Home'
-import Cart from './pages/Cart'
-import Login from './pages/Login'
-import ProductDetails from './pages/ProductDetails'
-
 function App() {
   return (
-    <>
-      <Navbar />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/cart" element={<Cart />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/product/:id" element={<ProductDetails />} />
-      </Routes>
-    </>
+    <div className="flex items-center justify-center h-screen bg-gray-100">
+      <h1 className="text-4xl font-bold text-blue-600">
+        Tailwind Working 🚀
+      </h1>
+    </div>
   )
 }
 
